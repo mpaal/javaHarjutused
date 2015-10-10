0160741a6d3e7682c2619ca23ae42f4fd467f3b5
Maris Paal <mpaal@klass1-12-l.intra.itcollege.ee>
2015-10-10 17:23:50 +0300

    praktikum 1

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
 
 /**
@@ -15,7 +17,41 @@ import lib.Foor;
  * pakub sulle, et kirjutab ise start() meetodi õigesti ära - proovi järgi.
  *
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
+ *
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        int count = 0;
+        while (count < 10) {
+
+            Foor foor = new Foor();
+            foor.vahetaPunast();
+            foor.paus(2);
+            foor.vahetaKollast();
+            foor.paus(0.6);
+            foor.vahetaPunast();
+            foor.vahetaKollast();
+            foor.vahetaRohelist();
+
+            foor.vahetaRohelist();
+            foor.paus(2);
+
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+
+            count = count +1;
+        }
+
+    }
 }
