a23894be6e2c2904054a4968ecc9d7df5f8ff967
Maris Paal <mpaal@klass1-12-l.intra.itcollege.ee>
2015-10-11 16:41:14 +0300

    peamurdja

--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -17,3 +17,43 @@ import java.util.Scanner;
  */
 public class Peamurdja3_laevad {
 }
+
+        public static void main(String[] args) {
+            int[]laud ={0,1,1,0,0,1,0,0,1};
+
+
+
+        Scanner kasutaja = new Scanner (System.in);
+
+        while (gameover(laud)) {
+            System.out.println(Arrays.toString(laud));
+
+            System.out.println("sisesta üks nr");
+            int sisestus = kasutaja.nextInt();
+
+            int hit = laud[sisestus];
+            if (hit == 1) {
+                System.out.println("tabamus");
+                laud[sisestus] = 2;
+
+            } else if (hit == 0) {
+
+                System.out.println("mööda");
+            } else if (hit == 2) {
+                System.out.println("juba põhjas");
+            }
+        }
+        System.out.println("Võitsid!");
+    }
+    public static boolean gameover(int[]laud){
+        for (int i=0; i<laud.length; i++) {
+            if (laud[i] == 1){
+                return true;
+            }
+        }
+        return false;
+
+
+
+    }
+}
